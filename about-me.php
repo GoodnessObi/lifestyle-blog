100784b3a9e31bab6d3bc3e3fc5332ce2522dbf9
goodness obi <goodnessobi@gmail.com>
2020-07-06 14:15:04 +0100

    serve html site with php

---
+++ about-me.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Living the social Life | About me</title>
+    <?php include('./partials/head.php') ?>
+</head>
+<body>
+    <?php $page = 'about-me'; include('./partials/navbar.php') ?>
+    <div class="container container-flex">
+        <main role="main">
+            <img src="https://images.unsplash.com/photo-1507691754536-1d2b755da326?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" alt="" class="image-full">
+            <h3>My name is Anne and living the simple life saved my life</h3>
+    
+            <p><strong>Lorem ipsum dolor sit amet</strong> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident</p>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    
+            <h3>How I turned things around</h3>
+            <p><strong>Lorem ipsum dolor sit amet, consectetur adipiscing elit,</strong> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident</p>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam</p>
+    
+            <h3>How I live the simple life</h3>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,<strong> sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</strong>. Excepteur sint occaecat cupidatat non proident</p>
+    
+        </main> 
+
+        <aside class="sidebar">
+            <div class="sidebar-widget">
+                <h2 class="widget-title">RECENT POSTS</h2>
+                <div class="widget-recent-post">
+                    <h3 class="widget-recent-post-title">Finding simplicity in life</h3>
+                    <img src="https://images.unsplash.com/photo-1534349762230-e0cadf78f5da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" alt="" class="widget-image">
+                </div>
+                <div class="widget-recent-post">
+                    <h3 class="widget-recent-post-title">Keeping cooking simple</h3>
+                    <img src="https://images.unsplash.com/photo-1455853739633-8c94c03d8121?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" alt="" class="widget-image">
+                </div>
+                <div class="widget-recent-post">
+                    <h3 class="widget-recent-post-title">Simple decorations</h3>
+                    <img src="https://images.unsplash.com/photo-1517705008128-361805f42e86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" alt="" class="widget-image">
+                </div>
+                <div class="widget-recent-post">
+                    <h3 class="widget-recent-post-title">Travelling simple</h3>
+                    <img src="https://images.unsplash.com/photo-1575429885953-bdd542481522?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="" class="widget-image">
+                </div>
+            </div>
+        </aside>
+    </div> 
+    <?php include('./partials/footer.php') ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Living the social Life | Home</title>
+		<?php include('./partials/head.php') ?>
+	</head>
+	<body>
+		<?php $page = 'home'; include('./partials/navbar.php') ?>
+		<div class="container container-flex">
+			<main role="main">
+				<article class="article-featured">
+					<h2 class="article-title">Finding simplicity in life</h2>
+					<img
+						src="https://images.unsplash.com/photo-1534349762230-e0cadf78f5da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+						alt=""
+						class="article-image"
+					/>
+					<p class="article-info">July 23, 2013 | 3 comments</p>
+					<p class="article-body">
+						<strong>Lorem ipsum dolor sit amet</strong>, consectetur adipiscing
+						elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+						aliqua. <a href="">Duis aute irure dolor in reprehenderit</a> Duis
+						aute irure dolor in reprehenderit in voluptate velit esse cillum
+						dolore eu fugiat nulla pariatur
+					</p>
+					<a href="" class="read-more">CONTINUE READING</a>
+				</article>
+				<article class="article-recent">
+					<div class="article-recent-main">
+						<h2 class="article-title">Keeping cooking simple</h2>
+						<p class="article-body">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							Excepteur sint occaecat cupidatat non proident.
+						</p>
+						<a href="" class="read-more">CONTINUE READING</a>
+					</div>
+					<div class="article-recent-secondary">
+						<img
+							src="https://images.unsplash.com/photo-1455853739633-8c94c03d8121?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+							alt=""
+							class="article-image"
+						/>
+						<p class="article-info">July 3, 2013 | 2 comments</p>
+					</div>
+				</article>
+				<article class="article-recent">
+					<div class="article-recent-main">
+						<h2 class="article-title">Simple decorations</h2>
+						<p class="article-body">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							Excepteur sint occaecat cupidatat non proident.
+						</p>
+						<a href="" class="read-more">CONTINUE READING</a>
+					</div>
+					<div class="article-recent-secondary">
+						<img
+							src="https://images.unsplash.com/photo-1517705008128-361805f42e86?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
+							alt=""
+							class="article-image"
+						/>
+						<p class="article-info">July 3, 2013 | 2 comments</p>
+					</div>
+				</article>
+				<article class="article-recent">
+					<div class="article-recent-main">
+						<h2 class="article-title">Travelling simple</h2>
+						<p class="article-body">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+							eiusmod tempor incididunt ut labore et dolore magna aliqua.
+							Excepteur sint occaecat cupidatat non proident.
+						</p>
+						<a href="" class="read-more">CONTINUE READING</a>
+					</div>
+					<div class="article-recent-secondary">
+						<img
+							src="https://images.unsplash.com/photo-1575429885953-bdd542481522?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+							alt=""
+							class="article-image"
+						/>
+						<p class="article-info">July 3, 2013 | 2 comments</p>
+					</div>
+				</article>
+			</main>
+			<?php include('./partials/sidebar.php') ?>
+
+		</div>
+		<?php include('./partials/footer.php') ?>
+	</body>
+</html>
